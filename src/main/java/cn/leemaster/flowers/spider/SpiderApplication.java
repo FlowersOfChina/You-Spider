1c91d336c7965c4b5473ef40b2be2e8e3932ff37
leemaster <leemast3r@outlook.com>
2018-03-03 19:57:23 +0800

    Init commit

---
+++ src/main/java/cn/leemaster/flowers/spider/SpiderApplication.java
@@ -0,0 +1,14 @@
+package cn.leemaster.flowers.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderApplication.class, args);
+	}
+}
